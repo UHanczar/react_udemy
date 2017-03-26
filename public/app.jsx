dd84cbcb46c98156fed2f985f467bd44b86d3f6e
Uladzimir <u.hanczar@gmail.com>
2017-03-26 23:41:12 +0300

    03.Creating_multiple_nested_components

--- public/app.jsx
+++ public/app.jsx
@@ -5,6 +5,42 @@
 let anotherName = 'Taya';
 let outerMassage = 'FROM OUTER STRING';
 
+const GreeterMessage = React.createClass({
+  render: function() {
+    const name = this.props.name;
+    const message = this.props.message;
+
+    return (
+      <div>
+        <h1>Hello {name}!</h1>
+        <p>{message}</p>
+      </div>
+    );
+  }
+});
+
+const GreeterForm = React.createClass({
+  onFormSubmit: function(e) {
+    e.preventDefault();
+
+    const name = this.refs.name.value;
+
+    if (name.length > 0) {
+      this.refs.name.value = '';
+      this.props.onNewName(name);
+    }
+  },
+
+  render: function() {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        <input type="text" ref="name" />
+        <button>Name</button>
+      </form>
+    );
+  }
+});
+
 const Greeter = React.createClass({
   // build-in function, which return object props: this.props ; is used when no variable was passed
   getDefaultProps: function() {
@@ -20,18 +56,10 @@ const Greeter = React.createClass({
     };
   },
 
-  onButtonClick: function(e) {
-    e.preventDefault();
-
-    let nameRef = this.refs.name;
-    let name = nameRef.value;
-    nameRef.value = '';
-
-    if(typeof name === 'string' && name.length > 0) {
-      this.setState({
-        name: name
-      });
-    }
+  handleNewName: function(name) {
+    this.setState({
+      name: name
+    })
   },
 
   render() {
@@ -40,14 +68,9 @@ const Greeter = React.createClass({
 
     return (
       <div className="container">
-        <h1>Hello, {name}</h1>
-        {/*we can use variable or props */}
-        <p>{`${this.props.message}!!`}</p>
+        <GreeterMessage name={name} message={message} />
 
-        <form onSubmit={this.onButtonClick}>
-          <input type="text" ref="name" />
-          <button>Name</button>
-        </form>
+        <GreeterForm onNewName={this.handleNewName} />
       </div>
     );
   }
