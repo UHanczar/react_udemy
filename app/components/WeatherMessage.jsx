aded7b1c17cd417d2bfb2004d4b39236344670df
Uladzimir <u.hanczar@gmail.com>
2017-04-04 23:45:59 +0300

    11.1.Definind_props_in_function

--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-const WeatherMessage = (props) => {
+const WeatherMessage = ({temp, location}) => {
+  {/* instead of defining variables here we can define then right in an object like in this module: instead of props we write vars names
   const {temp, location} = props;
+  */}
 
   return (
     <div>It is {temp} degree in {location}.</div>
